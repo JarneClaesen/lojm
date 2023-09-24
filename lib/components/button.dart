8da26ad53f8c0bef77fc39427b11e81948190916
JarneClaesenPXL <12004128@student.pxl.be>
2023-09-24 16:34:29 +0200

    Add login + register UI

---
+++ lib/components/button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final Function()? onTap;
+  final String text;
+  const MyButton({
+    Key? key,
+    required this.onTap,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        //height: 50,
+        //width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,84 @@
+import 'Package:flutter/material.dart';
+import 'package:orchestra_app/components/button.dart';
+import 'package:orchestra_app/components/text_field.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  // text editing controller
+  final emailTextController = TextEditingController();
+  final passwordTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 50),
+
+                //logo
+                const Icon(Icons.lock, size: 100),
+
+                const SizedBox(height: 50),
+
+                // welcome back message
+                Text(
+                  'Welcome Back!',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+
+                const SizedBox(height: 50),
+
+                // email textfield
+                MyTextField(controller: emailTextController, hintText: 'Email', obscureText: false),
+
+                const SizedBox(height: 10),
+
+                // password textfield
+                MyTextField(controller: passwordTextController, hintText: 'Password', obscureText: true),
+
+                const SizedBox(height: 25),
+
+                //signin button
+                MyButton(onTap: () {}, text: 'Sign in'),
+
+                const SizedBox(height: 10),
+
+                // go to register page
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Don\'t have an account?',
+                        style: TextStyle(
+                            color: Colors.grey[500]
+                        ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Register'),
+                    ),],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
