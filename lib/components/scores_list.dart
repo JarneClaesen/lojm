d21aba624268f1cec358c5db0c9334421f74f18a
JarneClaesenPXL <12004128@student.pxl.be>
2023-11-03 03:19:33 +0100

    Fix rectangular shadow behind score list item

--- lib/components/scores_list.dart
+++ lib/components/scores_list.dart
@@ -16,40 +16,47 @@ class ScoresList extends StatelessWidget {
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 25.0),
-          child: InkWell(
-            onTap: () async {
-              final localPath = await onDownload(pdfs[index]);
-              final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
+          child: ClipRRect(
+            borderRadius: StyleConstants.largeRoundedCorner,
+            child: Material(
+              borderRadius: StyleConstants.largeRoundedCorner,
+              color: Colors.transparent,
+              child: InkWell(
+                onTap: () async {
+                  final localPath = await onDownload(pdfs[index]);
+                  final isLandscape = MediaQuery.of(context).orientation == Orientation.landscape;
 
-              if (isLandscape) {
-                print("Tapped on PDF: $localPath");
-                onPdfSelected(localPath);
-              } else {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => PdfViewerPage(path: localPath),
-                  ),
-                );
-              }
-            },
-            child: Builder(
-              builder: (BuildContext innerContext) {
-                return Container(
-                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-                  decoration: BoxDecoration(
-                    borderRadius: StyleConstants.largeRoundedCorner,
-                    color: Theme.of(innerContext).colorScheme.primary,
-                  ),
-                  child: Row(
-                    children: [
-                      Icon(Icons.picture_as_pdf, color: Colors.red, size: 28),
-                      SizedBox(width: 20),
-                      Expanded(child: Text(pdfs[index], style: TextStyle(fontSize: 16))),
-                    ],
-                  ),
-                );
-              },
+                  if (isLandscape) {
+                    print("Tapped on PDF: $localPath");
+                    onPdfSelected(localPath);
+                  } else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PdfViewerPage(path: localPath),
+                      ),
+                    );
+                  }
+                },
+                child: Builder(
+                  builder: (BuildContext innerContext) {
+                    return Container(
+                      padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                      decoration: BoxDecoration(
+                        borderRadius: StyleConstants.largeRoundedCorner,
+                        color: Theme.of(innerContext).colorScheme.primary,
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(Icons.picture_as_pdf, color: Colors.red, size: 28),
+                          SizedBox(width: 20),
+                          Expanded(child: Text(pdfs[index], style: TextStyle(fontSize: 16))),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
             ),
           ),
         );
